e7244df0259cd558c0b275f44256fb4482266df8
Djordje Ilic <djordje.ilic@posteo.de>
2015-09-20 17:31:05 +0200

    fix readme

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
 
 # allow setup.py to be run from any path
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-sonic-screwdriver',
-    version='0.1.2',
+    version='0.1.2b0',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
