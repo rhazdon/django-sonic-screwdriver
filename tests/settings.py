7bfd7f97967c69439c76a5113e07fbceac01c6f9
Djordje Atlialp <djordje@atlialp.com>
2020-01-21 12:10:15 +0100

    Add test config

--- tests/settings.py
+++ tests/settings.py
@@ -1,8 +1,18 @@
+import os
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
 SECRET_KEY = "dummy"
 
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+DEBUG_PROPAGATE_EXCEPTIONS = True
+
+ROOT_URLCONF = "tests.urls"
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
@@ -10,3 +20,41 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
 ]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+]
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": (
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            )
+        },
+    }
+]
+
+DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3"}}
+
+USE_I18N = True
+USE_L10N = True
+
+TIME_ZONE = "UTC"
+LANGUAGE_CODE = "en-us"
+
+MEDIA_ROOT = ""
+MEDIA_URL = ""
+
+STATIC_URL = "/static/"
